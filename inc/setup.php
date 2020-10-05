88d330b8261d0d552d3ac40e9081d49dd50514d6
Josh Armentano <josh@abidewebdesign.com>
2020-10-05 13:13:27 -0700

    no message

--- inc/setup.php
+++ inc/setup.php
@@ -14,17 +14,26 @@ if( function_exists('acf_add_options_page') ) {
 	
 }
 
-add_action('admin_menu', 'remove_admin_posts' );
+add_action( 'wp_before_admin_bar_render', 'ocs_admin_bar_render' );
 
-function remove_admin_posts() {
+function ocs_admin_bar_render() {
+	
+    global $wp_admin_bar;
+	$wp_admin_bar->remove_menu('gappointments');
+	
+}
+
+add_action('admin_menu', 'ocs_remove_admin_posts' );
+
+function ocs_remove_admin_posts() {
 	
 	remove_menu_page('edit.php'); 
 
 }
 
-add_action( 'admin_init', 'rename_plugin_menus' );
+add_action( 'admin_init', 'ocs_rename_plugin_menus' );
 
-function rename_plugin_menus() {
+function ocs_rename_plugin_menus() {
 	
     global $menu;
 
