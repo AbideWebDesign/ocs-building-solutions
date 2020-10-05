7c9f585537db79e449a0e6937e6247b36bff4609
Josh Armentano <josh@abidewebdesign.com>
2020-10-05 13:12:01 -0700

    Updates to admin area

--- inc/setup.php
+++ inc/setup.php
@@ -13,3 +13,37 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page();
 	
 }
+
+add_action('admin_menu', 'remove_admin_posts' );
+
+function remove_admin_posts() {
+	
+	remove_menu_page('edit.php'); 
+
+}
+
+add_action( 'admin_init', 'rename_plugin_menus' );
+
+function rename_plugin_menus() {
+	
+    global $menu;
+
+    $updates = array(
+        "gAppointments" => array(
+            'name' => 'Schedule',
+        )
+    );
+
+    foreach ( $menu as $k => $props ) {
+
+        // Check for new values
+        $new_values = ( isset( $updates[ $props[0] ] ) ) ? $updates[ $props[0] ] : false;
+        
+        if ( ! $new_values ) continue;
+
+        // Change menu name
+        $menu[$k][0] = $new_values['name'];
+
+    }
+    
+}
